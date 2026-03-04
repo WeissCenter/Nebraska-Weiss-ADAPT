0f67107dbfb283b1c9a9766b084a683002e74dbd
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-03-04 14:12:28 -0500

    Add environment configuration for production settings

---
+++ apps/adapt-viewer/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  envLabel: 'Prod',
+  contentRoot: 'assets/text/generic',
+  contentFileName: 'viewer-content-text.json',
+  appDomain: 'ne-adapt-viewer.adatadata.org',
+  API_URL: 'https://v3yun4i0nc.execute-api.us-east-1.amazonaws.com/uat/',
+  logoPath: 'assets/shared/logos/generic',
+  logoExtension: 'svg',
+  copyrightText: 'The State of Nebraska.',
+};
