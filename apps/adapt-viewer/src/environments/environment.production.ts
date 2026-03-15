8544cddb9cae90b5f92212cfa6adb1a2d934e6f1
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-03-15 14:59:21 -0400

    logoExtension

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -5,7 +5,7 @@ export const environment = {
   appDomain: 'ne-adapt-viewer.adaptdata.org',
   API_URL: 'https://5mv6nkh30j.execute-api.us-east-1.amazonaws.com/ne-prod/',
   logoPath: 'assets/shared/logos/states/ne',
-  logoExtension: 'svg',
+  logoExtension: 'png',
   logoStyleClass: 'width-card',
   copyrightText: 'The State of Nebraska.',
 };
