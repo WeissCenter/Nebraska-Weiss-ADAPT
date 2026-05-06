64718f9a4ebaf7abfa0d753fafb7181a1427e429
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2026-05-06 14:35:28 -0400

    update domain

--- apps/adapt-viewer/src/environments/environment.production.ts
+++ apps/adapt-viewer/src/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   envLabel: 'Prod',
   contentRoot: 'assets/text/ne',
   contentFileName: 'viewer-content-text.json',
-  appDomain: 'ne-adapt-viewer.adaptdata.org',
+  appDomain: 'adapt-viewer.education.ne.gov',
   API_URL: 'https://5mv6nkh30j.execute-api.us-east-1.amazonaws.com/ne-prod/',
   logoPath: 'assets/shared/logos/states/ne',
   logoExtension: 'png',
